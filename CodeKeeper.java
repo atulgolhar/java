faa344cdd418573fca07d9109efbaeea86a8bce4
Atul Golhar <atulgolhar@gmail.com>
2017-01-06 14:57:37 -0500

    Create CodeKeeper.java

---
+++ CodeKeeper.java
@@ -0,0 +1,35 @@
+package com.java21days;
+
+import java.util.*;
+
+public class CodeKeeper {           // basic array list use
+    ArrayList list;
+    String[] codes = { "alpha", "lambda", "gamma", "delta", "zeta" };
+    
+    public CodeKeeper(String[] userCodes) {
+        list = new ArrayList();
+        
+        for (int i = 0; i < codes.length; i++) {        // load built-in codes
+            addCode(codes[i]);                          
+        }
+        
+        for (int j = 0; j < userCodes.length; j++) {    // load user cmd line codes
+            addCode(userCodes[j]);
+        }
+        
+        for (Iterator ite = list.iterator(); ite.hasNext(); ) {  //display all codes
+            String output = (String) ite.next();
+            System.out.println(output);
+        }
+    }
+    
+    private void addCode(String code) {
+        if (!list.contains(code)) {
+            list.add(code);
+        }
+    }
+    
+    public static void main(String[] arguments) {
+        CodeKeeper keeperCallItAnything = new CodeKeeper(arguments);
+    }
+}
