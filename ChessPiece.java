77d07dfdc7face28f65c8dd9e24e2338a638b7f1
Atul Golhar <atulgolhar@gmail.com>
2017-01-10 09:58:54 -0500

    Create ChessPiece.java

---
+++ ChessPiece.java
@@ -0,0 +1,19 @@
+package com.java21days;
+
+class ChessPiece {
+    String color;
+    int startingPosition;
+    int forwardMovement;
+    int sideMovement;
+}
+
+
+
+
+
+/*
+Create an inheritance hierarchy for the pieces of a chess set.
+Decide where instance variables should be defined in the
+hierarchy. Instance variables are: color, startingPosition, 
+forwardMovement, and sideMovement.
+*/
