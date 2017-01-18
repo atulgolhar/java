2bbeaffb27fe9327556833f1b94376dd876aa308
Atul Golhar <atulgolhar@gmail.com>
2017-01-18 13:07:08 -0500

    Create ByteReader.java

---
+++ ByteReader.java
@@ -0,0 +1,27 @@
+package com.java21days;         // application reads byte data and displays info
+
+import java.io.*;
+
+public class ByteReader {
+    public static void main(String[] arugments) {
+        try (
+                FileInputStream file = new FileInputStream("save.gif")
+            ) {
+            
+            boolean eof = false;
+            int count = 0;
+            while (!eof) {
+                int input = file.read();
+                System.out.print(input + " ");
+                if (input == -1)
+                    eof = true;
+                else
+                    count++;
+            }
+            file.close();
+            System.out.println("\nBytes read: " + count);
+        } catch (IOException e) {
+            System.out.println("Error -- " + e.toString());
+        }
+    }
+}
