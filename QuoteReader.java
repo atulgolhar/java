5d73cee0a9b314f3dc1ce5c062daca071055411c
Atul Golhar <atulgolhar@gmail.com>
2017-01-30 15:32:52 -0500

    Update QuoteReader.java

--- QuoteReader.java
+++ QuoteReader.java
@@ -1,5 +1,5 @@
 package com.java21days;                 // reads and displays records from Yahoo Finace stock quote database
-                                        // similar approach as QuoteData.java
+
 import java.sql.*;
 
 public class QuoteReader {
@@ -10,12 +10,12 @@ public class QuoteReader {
     }
     
     public void readStocks() {
-        String data = "jdbc:derby//localhost:1527/sample";
+        String data = "jdbc:derby://localhost:1527/sample";
                 try(
                     Connection conn = DriverManager.getConnection(data, "app", "app");
                     Statement st = conn.createStatement()) {
                     
-                    Class.forName("org.apache.derbyljdbc.ClientDriver");
+                    Class.forName("org.apache.derby.jdbc.ClientDriver");
                     
                     ResultSet rec = st.executeQuery(
                             "select TICKER, PRICE, DATE, CHANGE, LOW, HIGH, PRICEOPEN, VOLUME " +
@@ -25,11 +25,11 @@ public class QuoteReader {
                     while (rec.next()) {
                         System.out.println("DATE:\t" + rec.getString(3));
                         System.out.println("PRICE\t" + rec.getString(1));
-                        System.out.println("CHANGE\t" + rec.getString(2));
-                        System.out.println("LOW:\t" + rec.getString(4));
-                        System.out.println("HIGH:\t" + rec.getString(5));
-                        System.out.println("PRICEOPEN:\t" + rec.getString(6));
-                        System.out.println("VOLUME:\t" + rec.getString(7));
+                        System.out.println("CHANGE\t" + rec.getString(4));
+                        System.out.println("LOW:\t" + rec.getString(5));
+                        System.out.println("HIGH:\t" + rec.getString(6));
+                        System.out.println("PRICEOPEN:\t" + rec.getString(7));
+                        System.out.println("VOLUME:\t" + rec.getString(8));
                         System.out.println();
                         hasResults = true;
                     }
