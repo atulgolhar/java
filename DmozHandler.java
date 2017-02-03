f3b96271d5bdcdc4090476487d64e09796a18e87
Atul Golhar <atulgolhar@gmail.com>
2017-02-03 14:53:43 -0500

    Create DmozHandler.java

---
+++ DmozHandler.java
@@ -0,0 +1,7 @@
+package com.java21days;             // interface that defines the public methods that can
+                                    // be called remotely over XML-RPC
+import java.util.*;
+
+public interface DmozHandler {
+    public HashMap getRandomSite();
+}
